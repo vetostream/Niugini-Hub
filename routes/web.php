dc301e7c8e2a4426eab78558ff6dd0aef4313115
Tom <abaotom14@gmail.com>
2019-02-07 22:43:46 +0800

    add Orders Controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/products', 'ProductsController@index');
 Route::get('/stocks', 'StocksController@index');
+Route::get('/orders', 'OrdersController@index');
