972162f33ff2581bbef2ea08e162a038167ae048
Steven Joseph Ceniza <stevjocen@gmail.com>
2019-02-09 14:11:45 +0800

    Create migrations from ERD.

---
+++ database/migrations/2019_02_09_044148_create_sellers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('location');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->integer('products_sold');
+            $table->integer('products_posted');
+            $table->integer('products_count');
+            $table->float('stars', 1, 2);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
