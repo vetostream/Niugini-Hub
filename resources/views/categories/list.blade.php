c8ac38fe310d7a7c431ec9486504665d1eca19a0
Tom <abaotom14@gmail.com>
2019-02-10 22:00:49 +0800

    add categories

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -1,87 +1,26 @@
 @extends('layouts.base')
 
 @section('content')
-
-<!-- MAIN HEADER -->
-<div id="header">
+<!-- BREADCRUMB -->
+<div id="breadcrumb" class="section">
   <!-- container -->
   <div class="container">
     <!-- row -->
     <div class="row">
-
-      <!-- SEARCH BAR -->
-      <div class="col-md-9">
-        <div class="header-search">
-          <form method="POST" action="/search">
-            @csrf
-            <input class="input" placeholder="Search here">
-            <button class="search-btn">Search</button>
-          </form>
-        </div>
+      <div class="col-md-12">
+        <ul class="breadcrumb-tree">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">All Categories</a></li>
+          <li><a href="#">Accessories</a></li>
+          <li class="active">Headphones (227,490 Results)</li>
+        </ul>
       </div>
-      <!-- /SEARCH BAR -->
-
-      <!-- ACCOUNT -->
-      <div class="col-md-3 clearfix">
-        <div class="header-ctn">
-        @if (Auth::check())
-          <!-- Wishlist -->
-          <div>
-            <a href="{{ route('home') }}">
-              <i class="fa fa-heart-o"></i>
-              <span>Your Wishlist</span>
-            </a>
-          </div>
-          <!-- /Wishlist -->
-
-          <!-- Cart -->
-          <div>
-            <a href="{{ route('home') }}">
-              <i class="fa fa-shopping-cart"></i>
-              <span>Your Cart</span>
-            </a>
-          </div>
-          <!-- /Cart -->
-        @endif
-          <!-- Menu Toogle -->
-          <div class="menu-toggle">
-            <a href="#">
-              <i class="fa fa-bars"></i>
-              <span>Menu</span>
-            </a>
-          </div>
-          <!-- /Menu Toogle -->
-        </div>
-      </div>
-      <!-- /ACCOUNT -->
-
     </div>
-    <!-- row -->
-  </div>
-  <!-- container -->
-</div>
-<!-- /MAIN HEADER -->
-
-<!-- NAVIGATION -->
-<nav id="navigation">
-  <!-- container -->
-  <div class="container">
-    <!-- responsive-nav -->
-    <div id="responsive-nav">
-      <!-- NAV -->
-      <ul class="main-nav nav navbar-nav">
-        <li><a href="{{ route('home') }}">About Us</a></li>
-        <li><a href="{{ route('home') }}">Contact</a></li>
-        <li><a href="{{ route('home') }}">Profile</a></li>
-        <li><a href="{{ route('home') }}">Sell an Item</a></li>
-      </ul>
-      <!-- /NAV -->
-    </div>
-    <!-- /responsive-nav -->
+    <!-- /row -->
   </div>
   <!-- /container -->
-</nav>
-<!-- /NAVIGATION -->
+</div>
+<!-- /BREADCRUMB -->
 
 <!-- SECTION -->
 <div class="section">
@@ -89,17 +28,189 @@
   <div class="container">
     <!-- row -->
     <div class="row">
+      <!-- ASIDE -->
+      <div id="aside" class="col-md-3">
+        <!-- aside Widget -->
+        <div class="aside">
+          <h3 class="aside-title">Categories</h3>
+          <div class="checkbox-filter">
 
-      <!-- section title -->
-      <div class="col-md-12">
-        <div class="section-title">
-          <h3 class="title">Products</h3>
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-1">
+              <label for="category-1">
+                <span></span>
+                Laptops
+                <small>(120)</small>
+              </label>
+            </div>
+
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-2">
+              <label for="category-2">
+                <span></span>
+                Smartphones
+                <small>(740)</small>
+              </label>
+            </div>
+
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-3">
+              <label for="category-3">
+                <span></span>
+                Cameras
+                <small>(1450)</small>
+              </label>
+            </div>
+
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-4">
+              <label for="category-4">
+                <span></span>
+                Accessories
+                <small>(578)</small>
+              </label>
+            </div>
+
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-5">
+              <label for="category-5">
+                <span></span>
+                Laptops
+                <small>(120)</small>
+              </label>
+            </div>
+
+            <div class="input-checkbox">
+              <input type="checkbox" id="category-6">
+              <label for="category-6">
+                <span></span>
+                Smartphones
+                <small>(740)</small>
+              </label>
+            </div>
+          </div>
         </div>
+        <!-- /aside Widget -->
+
+        <!-- aside Widget -->
+        <div class="aside">
+          <h3 class="aside-title">Price</h3>
+          <div class="price-filter">
+            <div id="price-slider"></div>
+            <div class="input-number price-min">
+              <input id="price-min" type="number">
+              <span class="qty-up">+</span>
+              <span class="qty-down">-</span>
+            </div>
+            <span>-</span>
+            <div class="input-number price-max">
+              <input id="price-max" type="number">
+              <span class="qty-up">+</span>
+              <span class="qty-down">-</span>
+            </div>
+          </div>
+        </div>
+        <!-- /aside Widget -->
+
+        <!-- aside Widget -->
+        <div class="aside">
+          <h3 class="aside-title">Brand</h3>
+          <div class="checkbox-filter">
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-1">
+              <label for="brand-1">
+                <span></span>
+                SAMSUNG
+                <small>(578)</small>
+              </label>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-2">
+              <label for="brand-2">
+                <span></span>
+                LG
+                <small>(125)</small>
+              </label>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-3">
+              <label for="brand-3">
+                <span></span>
+                SONY
+                <small>(755)</small>
+              </label>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-4">
+              <label for="brand-4">
+                <span></span>
+                SAMSUNG
+                <small>(578)</small>
+              </label>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-5">
+              <label for="brand-5">
+                <span></span>
+                LG
+                <small>(125)</small>
+              </label>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" id="brand-6">
+              <label for="brand-6">
+                <span></span>
+                SONY
+                <small>(755)</small>
+              </label>
+            </div>
+          </div>
+        </div>
+        <!-- /aside Widget -->
+
+        <!-- aside Widget -->
+        <div class="aside">
+          <h3 class="aside-title">Top selling</h3>
+          <div class="product-widget">
+            <div class="product-img">
+              <img src="./img/product01.png" alt="">
+            </div>
+            <div class="product-body">
+              <p class="product-category">Category</p>
+              <h3 class="product-name"><a href="#">product name goes here</a></h3>
+              <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+            </div>
+          </div>
+
+          <div class="product-widget">
+            <div class="product-img">
+              <img src="./img/product02.png" alt="">
+            </div>
+            <div class="product-body">
+              <p class="product-category">Category</p>
+              <h3 class="product-name"><a href="#">product name goes here</a></h3>
+              <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+            </div>
+          </div>
+
+          <div class="product-widget">
+            <div class="product-img">
+              <img src="./img/product03.png" alt="">
+            </div>
+            <div class="product-body">
+              <p class="product-category">Category</p>
+              <h3 class="product-name"><a href="#">product name goes here</a></h3>
+              <h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+            </div>
+          </div>
+        </div>
+        <!-- /aside Widget -->
       </div>
-      <!-- /section title -->
+      <!-- /ASIDE -->
 
       <!-- STORE -->
-      <div id="store" class="col-md-12">
+      <div id="store" class="col-md-9">
+
         <!-- store products -->
         <div class="row">
           <!-- product -->
@@ -366,6 +477,18 @@
         </div>
         <!-- /store products -->
 
+        <!-- store bottom filter -->
+        <div class="store-filter clearfix">
+          <span class="store-qty">Showing 20-100 products</span>
+          <ul class="store-pagination">
+            <li class="active">1</li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+          </ul>
+        </div>
+        <!-- /store bottom filter -->
       </div>
       <!-- /STORE -->
     </div>
@@ -374,5 +497,4 @@
   <!-- /container -->
 </div>
 <!-- /SECTION -->
-
 @endsection
