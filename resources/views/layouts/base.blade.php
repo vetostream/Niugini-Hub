443a74bfbbe9fa200fd25e35ba49289ee6111b3a
Tom <abaotom14@gmail.com>
2019-02-10 21:36:01 +0800

    edit register ui

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -28,7 +28,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Niugini-hub') }}</title>
+  <title>Niugini-hub</title>
 </head>
 <body>
   <!-- HEADER -->
@@ -36,6 +36,9 @@
     <!-- TOP HEADER -->
     <div id="top-header">
       <div class="container">
+        <ul class="header-links pull-left">
+          <li><a href="/">Home</li>
+        </ul>
         <ul class="header-links pull-right">
         <!-- Authentication Links -->
         @guest
@@ -50,66 +53,6 @@
       </div>
     </div>
     <!-- /TOP HEADER -->
-
-    <!-- MAIN HEADER -->
-    <div id="header">
-      <!-- container -->
-      <div class="container">
-        <!-- row -->
-        <div class="row">
-
-          <!-- SEARCH BAR -->
-          <div class="col-md-9">
-            <div class="header-search">
-              <form method="POST" action="/search">
-                @csrf
-                <input class="input" placeholder="Search here">
-                <button class="search-btn">Search</button>
-              </form>
-            </div>
-          </div>
-          <!-- /SEARCH BAR -->
-
-          <!-- ACCOUNT -->
-          <div class="col-md-3 clearfix">
-            <div class="header-ctn">
-            @if (Auth::check())
-              <!-- Wishlist -->
-              <div>
-                <a href="{{ route('home') }}">
-                  <i class="fa fa-heart-o"></i>
-                  <span>Your Wishlist</span>
-                </a>
-              </div>
-              <!-- /Wishlist -->
-
-              <!-- Cart -->
-              <div>
-                <a href="{{ route('home') }}">
-                  <i class="fa fa-shopping-cart"></i>
-                  <span>Your Cart</span>
-                </a>
-              </div>
-              <!-- /Cart -->
-            @endif
-              <!-- Menu Toogle -->
-              <div class="menu-toggle">
-                <a href="#">
-                  <i class="fa fa-bars"></i>
-                  <span>Menu</span>
-                </a>
-              </div>
-              <!-- /Menu Toogle -->
-            </div>
-          </div>
-          <!-- /ACCOUNT -->
-
-        </div>
-        <!-- row -->
-      </div>
-      <!-- container -->
-    </div>
-    <!-- /MAIN HEADER -->
 
   </header>
   <!-- /HEADER -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,66 @@
 
 @section('content')
 
+<!-- MAIN HEADER -->
+<div id="header">
+  <!-- container -->
+  <div class="container">
+    <!-- row -->
+    <div class="row">
+
+      <!-- SEARCH BAR -->
+      <div class="col-md-9">
+        <div class="header-search">
+          <form method="POST" action="/search">
+            @csrf
+            <input class="input" placeholder="Search here">
+            <button class="search-btn">Search</button>
+          </form>
+        </div>
+      </div>
+      <!-- /SEARCH BAR -->
+
+      <!-- ACCOUNT -->
+      <div class="col-md-3 clearfix">
+        <div class="header-ctn">
+        @if (Auth::check())
+          <!-- Wishlist -->
+          <div>
+            <a href="{{ route('home') }}">
+              <i class="fa fa-heart-o"></i>
+              <span>Your Wishlist</span>
+            </a>
+          </div>
+          <!-- /Wishlist -->
+
+          <!-- Cart -->
+          <div>
+            <a href="{{ route('home') }}">
+              <i class="fa fa-shopping-cart"></i>
+              <span>Your Cart</span>
+            </a>
+          </div>
+          <!-- /Cart -->
+        @endif
+          <!-- Menu Toogle -->
+          <div class="menu-toggle">
+            <a href="#">
+              <i class="fa fa-bars"></i>
+              <span>Menu</span>
+            </a>
+          </div>
+          <!-- /Menu Toogle -->
+        </div>
+      </div>
+      <!-- /ACCOUNT -->
+
+    </div>
+    <!-- row -->
+  </div>
+  <!-- container -->
+</div>
+<!-- /MAIN HEADER -->
+
 <!-- NAVIGATION -->
 <nav id="navigation">
   <!-- container -->
@@ -10,7 +70,6 @@
     <div id="responsive-nav">
       <!-- NAV -->
       <ul class="main-nav nav navbar-nav">
-        <li class="active"><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('home') }}">About Us</a></li>
         <li><a href="{{ route('home') }}">Contact</a></li>
         <li><a href="{{ route('home') }}">Profile</a></li>
