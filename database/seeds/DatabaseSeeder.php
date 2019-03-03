52a89a76b513b205c3fdc5f1cd0e5c0e2d474e0d
Tom <abaotom14@gmail.com>
2019-03-03 09:35:18 +0800

    edit category image display

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(SellersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
     }
 }
