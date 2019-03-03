bdf863da5896a749f7a7dd91f11cb5cdf49236a5
Tom <abaotom14@gmail.com>
2019-03-03 09:44:49 +0800

    edit products seeder and display

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(SellersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
