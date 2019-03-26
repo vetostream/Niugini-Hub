a8b863ca16debdb735fdb016248fd123840d4a62
Stevie <stevjocen@gmail.com>
2019-03-26 21:10:48 +0800

    Update some models.

--- app/Sellers.php
+++ app/Sellers.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Orders;
 
 class Sellers extends Model
 {
@@ -19,6 +20,27 @@ class Sellers extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the products associated with the seller.
+     */
+    public function products()
+    {
+        return $this->hasMany('App\Products');
+    }
+
+    /**
+     * Get the products ordered associated with the seller.
+     */
+    public function orders()
+    {
+        $product_ids = $this->products->pluck('id');
+
+        return Orders::whereHas('products', function($query) use($product_ids)
+        {
+            $query->whereIn('products_id', $product_ids);
+        })->get();
     }
 
 }
