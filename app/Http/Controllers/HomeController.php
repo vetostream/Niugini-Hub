47252b7e1c265127af834290cc1c24a4bee3a1c2
Stevie <stevjocen@gmail.com>
2019-03-26 18:54:34 +0800

    Edit pagination.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $products = Products::paginate(12);
+        $products = Products::where('status', 1)->paginate(12);
         return view('home', ['products' => $products]);
     }
 
