f8f385d5eff7e4f19a8c30f0746d35d05106b111
Steven Joseph S. Ceniza <ceniza.sjs@AD.SPROBE.PH>
2019-03-28 10:52:06 +0800

    Test pusher.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Products as Products;
 use App\Categories;
+use App\Events\SellerRequestsUpdate;
 
 class HomeController extends Controller
 {
@@ -26,6 +27,7 @@ class HomeController extends Controller
     public function index()
     {
         $products = Products::where('status', 1)->paginate(12);
+        SellerRequestsUpdate::dispatch();
         return view('home', ['products' => $products]);
     }
 
